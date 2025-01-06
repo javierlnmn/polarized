ca757ca65ceba0f40e67202156ee9c7558226716
javierlnmn <jllm03123@gmail.com>
2025-01-06 13:45:48 +0100

    Voting template and view

--- subjects/urls.py
+++ subjects/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import SubjectListView
+from django.views.generic.base import RedirectView
+
+from .views import SubjectListView, VotingView
 
 
 app_name = 'subjects'
 
 urlpatterns = [
-    path('', SubjectListView.as_view(), name='subjects_list'),
+    path('', RedirectView.as_view(url='/subjects/', permanent=True), name='home'),
+    path('subjects/', SubjectListView.as_view(), name='subjects_list'),
+    path('subjects/<int:subject_id>/vote/', VotingView.as_view(), name='subject_vote'),
 ]
--- subjects/views.py
+++ subjects/views.py
@@ -1,9 +1,43 @@
-from django.views.generic import ListView
-from .models import Subject
+from django.http.response import HttpResponse
+from django.views.generic import ListView, View
+from django.shortcuts import get_object_or_404, render
+
+from .models import Subject, Vote
 
 
 class SubjectListView(ListView):
     model = Subject
     template_name = 'subjects/subjects_list.html'
-    # context_object_name = 'page_obj'
     paginate_by = 5
+
+
+class VotingView(View):
+
+    def post(self, request, subject_id):
+        subject = get_object_or_404(Subject, id=subject_id)
+
+        vote_value_str = request.POST.get('value')
+
+        try: 
+            vote_value = int(vote_value_str)
+        except TypeError:
+            return HttpResponse(status=400)
+
+        if vote_value not in dict(Vote.VoteChoices.choices):
+            return HttpResponse(status=400)
+
+        vote, created = Vote.objects.get_or_create(
+            subject=subject,
+            user=request.user,
+            defaults={'value': vote_value}
+        )
+
+        if not created and vote.value != vote_value:
+            vote.value = vote_value
+            vote.save()
+
+        return render(request, 'subjects/components/subject_voting_item.html', {
+            'subject': subject,
+            'voted': True,
+            'result_data': subject.result,
+        })
