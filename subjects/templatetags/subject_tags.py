6788574a8926590f36abf428b29800c82b7dfccd
javierlnmn <jllm03123@gmail.com>
2025-01-07 16:17:36 +0100

    User voted property in subject model

---
+++ subjects/templatetags/subject_tags.py
@@ -0,0 +1,10 @@
+from django import template
+
+
+register = template.Library()
+
+@register.filter
+def user_voted(subject, user):
+    if not user.is_authenticated:
+        return False
+    return subject.user_voted(user)
